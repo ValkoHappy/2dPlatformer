dded1b53add1114410b13a3ddf6334388d6eb151
ValkoHappy <11081108cat@gmail.com>
2022-08-14 13:51:47 +0300

    1

---
+++ Assets/Skripts/AnimatorControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorControl : MonoBehaviour
+{
+    private Animator _animator;
+
+    public bool IsMoving { private get;  set; }
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        _animator.SetBool("IsMoving", IsMoving);
+    }
+}
---
+++ Assets/Skripts/Coin.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _entered;
+
+    private AnimatorControl _animator;
+    private bool _isRunning;
+
+    private void Start()
+    {
+        _animator = GetComponentInChildren<AnimatorControl>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            _entered?.Invoke();
+        }
+    }
+}
